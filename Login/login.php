a83d450828e4ba8da0bc63ba607fee3af661e38f
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-01-22 22:15:46 +0700

    Push lagi nih

---
+++ Login/login.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../assets/font/icomoon/style.css">
+
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="../assets/css/login.css">
+
+    <title>LOGIN VOTE</title>
+</head>
+
+<body>
+
+
+
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="../assets/images/bg/bg-login.png" alt="Image" class="img-fluid" width="600" height="600">
+                </div>
+                <div class="col-md-6 contents">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="mb-4">
+                                <h3 class="text-center">LOGIN HERE</h3>
+                                <p class="mb-4 text-center">Universitas Logistik dan Bisnis Internasinal.</p>
+                            </div>
+                            <form action="#" method="post">
+                            <label for="username" class="form-label">Username</label>
+                                <div class="form-group first">
+                                    <label for="username">Enter your username</label>
+                                    <input type="text" class="form-control" id="username">
+
+                                </div>
+                                <br>
+                                <label for="email" class="form-label">Email</label>
+                                <div class="form-group first">
+                                    <label for="username">Enter your email</label>
+                                    <input type="text" class="form-control" id="email">
+
+                                </div>
+                                <br>
+                                <label for="password" class="form-label">Password</label>
+                                <div class="form-group last mb-4">
+                                    <label for="password">Enter your password</label>
+                                    <input type="password" class="form-control" id="password">
+
+                                </div>
+                                <hr>
+                                <div class=" mb-5 align-items-center">
+                                    <p class="text-center">Don't have an account? <a href="register.php"> Register</a> here.</p>
+                                    <p class="text-center"><a href="../index.php">Back</a></p>
+                                </div>
+                                <input type="submit" value="Log In" class="btn btn-block btn-primary">
+
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <script src="../assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../assets/js/popper.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+</body>
+
+</html>
